2f58b77a65a27be9b0e568b624d91438ee89a495
cyrus01337 <cyrus21337@gmail.com>
2022-07-26 16:38:40 +0100

    Support following symlink

--- build.sh
+++ build.sh
@@ -18,20 +18,20 @@ rm -rf $build_dir && mkdir -p $build_dir
 
 for f in *prerun.sh home
 do
-    cp -r $CDIR/$f $build_dir/
+    cp -Lr $CDIR/$f $build_dir/
 done
 
 if [ -x "$(command -v pip)" ]; then
 
   PYTHONUSERBASE=$build_dir/home/.local pip install --user -I -r pip-requirements.txt
-  
+
   # Fix python shebang
   pypath=`readlink -f $(which python)`
   if [ -d "$build_dir/home/.local/bin" ]; then
     echo 'Fix PyPi packages shebang'
     sed -i '1s|#!'$pypath'|#!/usr/bin/env python|' $build_dir/home/.local/bin/*
   fi
-  
+
 else
   echo 'Skip pip packages installation: pip not found.'
 fi
